a1e60911a6452cd96a6a5634c2bfc3f930a516d1
Denis Sitdikov <denis.sitd@yandex.ru>
2023-02-06 10:03:05 +0300

    ref

--- src/script.py
+++ src/script.py
@@ -1,5 +1,14 @@
 from renamer import Renamer
 from utils import get_config
+from time import sleep
 
 rn = Renamer(get_config('config.yaml'))
 rn.rename()
+
+print('done!')
+sleep(10)
+print('closing')
+for i in range(5,1):
+    print(i)
+    sleep(1)
+
